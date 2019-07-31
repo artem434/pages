c1aa269c55204e2506af5a5925b4a24241aa3a69
karachevtsev <karachevtsev@yahoo.com>
2019-07-31 12:51:14 +0300

    Added dtkt page

---
+++ src/dtkt/src/component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="page-dtkt">
+        <div class="page-dtkt">
+            <div class="container">
+                <h1>page-dtkt</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Page from '../../core/Page'
+
+export default {
+    mixins: [Page],
+}
+</script>

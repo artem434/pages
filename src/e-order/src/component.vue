96cc377a4707d03aa481b3cb62092f3b4003c153
karachevtsev <karachevtsev@yahoo.com>
2019-08-02 16:35:41 +0300

    Added desktop order

--- src/e-order/src/component.vue
+++ src/e-order/src/component.vue
@@ -1,94 +1,153 @@
 <template>
     <div id="page-order">
-        <router-link :to="{
-        name:'page',
-        params:{page:'order'},
-        query:{
-            article:'ep-interbuh_gazeta',
-            'options[periods]':3,
-            'options[begin]': '2020-01-01',
-        }
-        }" replace>3
-        </router-link>
-        ,
-        <router-link :to="{
-        name:'page',
-        params:{page:'order'},
-        query:{
-            article:'ep-interbuh_gazeta',
-            'options[periods]':6,
-            'options[begin]': '2020-01-01',
-        }
-        }" replace>6
-        </router-link>
-        ,
-        <router-link :to="{
-        name:'page',
-        params:{page:'order'},
-        query:{
-            article:'ep-interbuh_gazeta',
-            'options[periods]':12,
-            'options[begin]': '2020-01-01',
-        }
-        }" replace>12
-        </router-link>
-        <div v-if="processing">
-            Processing
-        </div>
-        <div v-if="loading">
-            Loading
-        </div>
-        <div v-else>
-            <div>{{product.title}}</div>
-            <div>Ціна: {{price.pretty}}</div>
-            <div v-if="options.begin">
-                Початок:
-                <span v-if="product.options.beginValues && product.options.beginValues.length > 1">
-                    <select v-model="options.begin">
-                        <option v-for="value in product.options.beginValues" :key="value.value"
-                                :value="value.value"
-                                v-text="value.label"></option>
-                    </select>
-                </span>
-                <span v-else>{{options.begin}}</span>
-            </div>
-            <div v-if="options.periods">
-                Період:
-                <span v-if="product.options.periodValues && product.options.periodValues.length > 1">
-                    <select v-model="options.periods">
-                        <option v-for="value in product.options.periodValues" :key="value"
-                                :value="value"
-                                v-text="value"></option>
-                    </select>
-                </span>
-                <span v-else>{{options.periods}}</span>
-            </div>
-            <template v-if="product.hasQuantity">
-                <div>Кількість: <input v-model="quantity"/></div>
-                <div>Cума: {{price.prettyTotal}}</div>
-            </template>
+        <div class="container">
+            <div class="e-order">
+                <div v-if="processing" class="e-order-loader">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+                <div v-if="loading" class="e-order-loader">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+                <div v-else>
+                    <div class="order">
+                        <h1 class="order__headline">Інформація про замовлення:</h1>
+                        <div class="order__info-product">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Назва:</label>
+                                <div class="col-sm-9 col-form-value"><b>{{product.title}}</b></div>
+                            </div>
+                            <div class="form-group row align-items-center">
+                                <label class="col-sm-3 col-form-label">Сума:</label>
+                                <div class="col-sm-9">{{price.pretty}} грн</div>
+                            </div>
+                            <div v-if="options.begin" class="form-group row align-items-center">
+                                <label class="col-sm-3 col-form-label">Початок:</label>
+                                <div class="col-sm-9">
+                                        <span v-if="product.options.beginValues && product.options.beginValues.length > 1">
+                                            <select class="form-control" v-model="options.begin">
+                                                <option v-for="value in product.options.beginValues" :key="value.value"
+                                                        :value="value.value"
+                                                        v-text="value.label"></option>
+                                            </select>
+                                        </span>
+                                    <span v-else>{{options.begin}}</span>
+                                </div>
+                            </div>
+                            <div v-if="options.periods" class="form-group row align-items-center">
+                                <label class="col-sm-3 col-form-label">Період:</label>
+                                <div class="col-sm-9">
+                                        <span v-if="product.options.periodValues && product.options.periodValues.length > 1">
+                                            <select class="form-control" v-model="options.periods">
+                                                <option v-for="value in product.options.periodValues" :key="value"
+                                                        :value="value"
+                                                        v-text="value">
+                                                </option>
+                                            </select>
+                                        </span>
+                                    <span v-else>{{options.periods}}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-if="product.hasQuantity" class="order__quantity align-items-center">
+                            <template>
+                                <div>Кількість: <input v-model="quantity"/></div>
+                                <div>Cума: {{price.prettyTotal}} грн</div>
+                            </template>
+                        </div>
+                        <div class="order__total align-items-center">
+                            <span class="wallet">Загальна сума:</span>
+                            <span><b>{{price.pretty}}</b> грн</span>
+                        </div>
+                        <div class="order__info-payer">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">E-mail:</label>
+                                <div class="col-sm-9">
+                                    <input v-model="customer.email" class="form-control"/>
+                                    <span class="error" v-if="errors.has('customer.email')">{{errors.first('customer.email')}}</span>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Платник:</label>
+                                <div class="col-sm-9">
+                                    <input v-model="customer.name" class="form-control"/>
+                                    <span class="error" v-if="errors.has('customer.name')">{{errors.first('customer.name')}}</span>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Телефон:</label>
+                                <div class="col-sm-9">
+                                    <input v-model="customer.phone" class="form-control"/>
+                                    <span class="error" v-if="errors.has('customer.phone')">{{errors.first('customer.phone')}}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="order__controls align-items-center">
+                            <div v-if="product.processing.includes('INVOICE')">
+                                <button @click="send('INVOICE')" class="btn btn-order btn-form">Отримати рахунок
+                                </button>
+                            </div>
+                            <div v-if="product.processing.includes('CARD')">
+                                <button @click="send('CARD')" class="btn btn-form btn-card">Оплатити карткою</button>
+                            </div>
+                        </div>
+                        <div class="order__logo"></div>
+                    </div>
+                </div>
 
-            <div>
-                <div>email <input v-model="customer.email"/>
-                    <span v-if="errors.has('customer.email')">{{errors.first('customer.email')}}</span></div>
-                <div>Ім'я <input v-model="customer.name"/>
-                    <span v-if="errors.has('customer.name')">{{errors.first('customer.name')}}</span></div>
-                <div>Телефон <input v-model="customer.phone"/>
-                    <span v-if="errors.has('customer.phone')">{{errors.first('customer.phone')}}</span></div>
-            </div>
-            <div v-if="product.processing.includes('INVOICE')">
-                <button @click="send('INVOICE')">Рахунок</button>
-            </div>
-            <div v-if="product.processing.includes('CARD')">
-                <button @click="send('CARD')">Картка</button>
-            </div>
-        </div>
-        <div class="popup" v-if="done">
-            done
-        </div>
-        <div v-if="liqpay" id="page-order-liqpay-wrapper">
-            <div id="page-order-liqpay">
-                <div class="close" @click="liqpay=false">x</div>
+                <!--<router-link :to="{-->
+                <!--name:'page',-->
+                <!--params:{page:'order'},-->
+                <!--query:{-->
+                <!--article:'ep-interbuh_gazeta',-->
+                <!--'options[periods]':3,-->
+                <!--'options[begin]': '2020-01-01',-->
+                <!--}-->
+                <!--}" replace>3-->
+                <!--</router-link>-->
+                <!--,-->
+                <!--<router-link :to="{-->
+                <!--name:'page',-->
+                <!--params:{page:'order'},-->
+                <!--query:{-->
+                <!--article:'ep-interbuh_gazeta',-->
+                <!--'options[periods]':6,-->
+                <!--'options[begin]': '2020-01-01',-->
+                <!--}-->
+                <!--}" replace>6-->
+                <!--</router-link>-->
+                <!--,-->
+                <!--<router-link :to="{-->
+                <!--name:'page',-->
+                <!--params:{page:'order'},-->
+                <!--query:{-->`
+                <!--article:'ep-interbuh_gazeta',-->
+                <!--'options[periods]':12,-->
+                <!--'options[begin]': '2020-01-01',-->
+                <!--}-->
+                <!--}" replace>12-->
+                <!--</router-link>-->
+                <div v-if="done" class="popup">
+                    <div class="popup-body">
+                        <h2>Дякуємо!</h2>
+                        <p>Вам відправлено лист з рахунком</p>
+                        <app-logo/>
+                        <span class="popup-close"></span>
+                    </div>
+                </div>
+
+                <div v-if="liqpay" id="page-order-liqpay-wrapper">
+                    <div id="page-order-liqpay">
+                        <div class="close" @click="liqpay=false">x</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -97,9 +156,11 @@
 <script>
 import Errors from 'form-backend-validation/src/Errors'
 import liqpayFactory from './liqpay'
+import AppLogo from './AppLogo'
 
-const fill = (name, data) => Object.keys(data).reduce((obj, key) => Object.assign(obj, { [`${name}[${key}]`]: data[key] }), {})
+const fill = (name, data) => Object.keys(data).reduce((obj, key) => Object.assign(obj, {[`${name}[${key}]`]: data[key]}), {})
 export default {
+    components: { AppLogo },
     props: {
         page: {
             type: Object,
@@ -132,7 +193,7 @@ export default {
             params: {
                 ...to.query,
             },
-        }).then(({ data }) => {
+        }).then(({data}) => {
             next(vm => vm.setData(data))
         }, next)
     },
@@ -153,7 +214,7 @@ export default {
         },
     },
     methods: {
-        setData({ price, quantity, ...product }) {
+        setData({price, quantity, ...product}) {
             this.price = price
             this.product = product
             this.quantity = quantity
@@ -171,7 +232,7 @@ export default {
                     quantity: this.quantity,
                     ...fill('options', this.options),
                 },
-            }).then(({ data }) => {
+            }).then(({data}) => {
                 this.price = data
                 this.loadingPrice = false
             })
@@ -184,7 +245,7 @@ export default {
                 processing,
                 options: this.options,
                 customer: this.customer,
-            }).then(({ data: result }) => {
+            }).then(({data: result}) => {
                     switch (result.action) {
                         case 'AWAIT':
                             this.done = true
@@ -246,8 +307,3 @@ export default {
     },
 }
 </script>
-<style lang="scss">
-    #page-order {
-
-    }
-</style>

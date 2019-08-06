b935a425d58dedac6de53193ef6abc62f0da20e9
karachevtsev <karachevtsev@yahoo.com>
2019-08-06 19:27:52 +0300

    Fixed dtkt page

--- src/dtkt/src/Advantages.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <slider id="advantages_slider" name="epressa_add_dtkt" class="row">
-        <template v-slot="{slides, current, prev, next, to}">
-            <div class="col-sm-12 col-md-6 col-lg-4 advantages__item" v-for="slide, index in slides" :key="index">
-                <div class="advantage" v-html="slide.options.caption"></div>
-            </div>
-        </template>
-    </slider>
-</template>
--- src/dtkt/src/component.vue
+++ src/dtkt/src/component.vue
@@ -1,182 +1,119 @@
 <template>
-    <div id="page-dtkt">
-        <div class="landing">
-            <div class="landing-section">
-                <div class="container">
-                    <div class="landing-cover">
-                        <picture class="landing-cover">
-                            <source :srcset="mix('images/cover.webp')" type="image/webp">
-                            <img :src="mix('images/cover.jpg')" class="img-fluid" alt="">
-                        </picture>
-                    </div>
-                </div>
-            </div>
-            <section class="landing-section">
-                <div class="container">
-                    <h1 class="landing-section__title landing-title"><span>Про видання</span></h1>
-                    <div class="about">
-                        <div class="row">
-                            <div class="col-md-6 col-lg-8 about__text">
-                                <p>«Дебет-Кредит» – бухгалтерський сервіс №1, який об'єднує бухгалтерів з усієї України. Щотижня він пропонує підбірку аналітичних статей, підготовлених висококваліфікованими фахівцями редакції, бухгалтерами-практиками, юристами, фахівцями держорганів. У ньому регулярно з’являється інформація про нові закони, нормативно-правові акти з докладними коментарями та практичними рекомендаціями. І разом з тим сервіс надає доступ до сотень готових відповідей у базі онлайн-консультацій. «Дебет-Кредит» – це джерело вашої абсолютної впевненості у власних діях.
-                                </p>
-                            </div>
-                            <div class="col-md-6 col-lg-4 about__image text-center d-none d-sm-block">
-                                <source :srcset="mix('images/about.webp')" type="image/webp">
-                                <img :src="mix('images/about.jpg')" class="img-fluid" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section id="order"  class="landing-section">
-                <div class="container">
-                    <h2 class="landing-section__title landing-title"><span>Ціни</span></h2>
-                    <p class="landing-section__action">Акційна пропозиція діє до 19 липня</p>
-                    <div class="order">
-                        <div class="row justify-content-center">
-                            <div class="col-sm-6 col-md-4 col-lg-3 order__package">
-                               <package article="ep-dtkt_tr">
-                                    <template v-slot:title>ПАКЕТ <br> «ТЕМАТИЧНІ РОЗСИЛКИ»</template>
-                                    <template v-slot:gifts>
-                                        <li>Відеозапис Великого бухгалтерського семінару</li>
-                                        <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
-                                    </template>
-                                    <template v-slot:abilities>
-                                        <li>Регулярні періодичні добірки – у вашій поштовій скрині</li>
-                                        <li>Обирайте з 12 можливих тем те, що вам зараз дійсно потрібно</li>
-                                    </template>
-                                </package>
-                            </div>
-                            <div class="col-sm-6 col-md-4 col-lg-3 order__package">
-                                <package article="ep-dtkt_ma" class="dddd">
-                                    <template v-slot:title>ПАКЕТ <br> «МІЙ АСИСТЕНТ»</template>
-                                    <template v-slot:gifts>
-                                        <li>Відеозапис Великого бухгалтерського семінару</li>
-                                        <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
-                                    </template>
-                                    <template v-slot:abilities>
-                                        <li>Щоденна добірка новин</li>
-                                        <li>«Документи за тиждень»</li>
-                                        <li>«Запитання і відповіді» за тиждень</li>
-                                        <li>Розумний податковий календар</li>
-                                        <li>База нормативних документів</li>
-                                        <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
-                                        <li>Сервіс «Мій кабінет»</li>
-                                    </template>
-                                </package>
-                            </div>
-                            <div class="col-sm-6 col-md-4 col-lg-3 order__package">
-                                <package article="ep-dtkt_ek">
-                                    <template v-slot:title>ПАКЕТ <br> «ЕКСПЕРТ»</template>
-                                    <template v-slot:gifts>
-                                        <li>Відеозапис Великого бухгалтерського семінару</li>
-                                        <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
-                                    </template>
-                                    <template v-slot:abilities>
-                                        <li>Щоденна добірка новин</li>
-                                        <li>«Документи за тиждень»</li>
-                                        <li>«Запитання і відповіді» за тиждень</li>
-                                        <li>Розумний податковий календар</li>
-                                        <li>База нормативних документів</li>
-                                        <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
-                                        <li>Сервіс «Мій кабінет»</li>
-                                        <li>Пакет «Тематичні розсилки»</li>
-                                        <li>Електронний журнал «Дебет-Кредит»</li>
-                                        <li>Щомісячні спецвипуски</li>
-                                        <li>Доступ до архівних номерів на сайті</li>
-                                    </template>
-                                </package>
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-md-4 col-lg-3 order__package">
-                                <package article="ep-dtkt_pr" class="dddd">
-                                    <template v-slot:title>ПАКЕТ <br> «ПРОФІ»</template>
-                                    <template v-slot:gifts>
-                                        <li>Відеозапис Великого бухгалтерського семінару</li>
-                                        <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
-                                    </template>
-                                    <template v-slot:abilities>
-                                        <li>Щоденна добірка новин</li>
-                                        <li>«Документи за тиждень»</li>
-                                        <li>«Запитання і відповіді» за тиждень</li>
-                                        <li>Розумний податковий календар</li>
-                                        <li>База нормативних документів</li>
-                                        <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
-                                        <li>Сервіс «Мій кабінет»</li>
-                                        <li>Пакет «Тематичні розсилки»</li>
-                                        <li>Електронний журнал «Дебет-Кредит»</li>
-                                        <li>Щомісячні спецвипуски</li>
-                                        <li>Доступ до архівних номерів на сайті</li>
-                                        <li>Сервіс «Постав запитання редакції»</li>
-                                        <li>Вебінари редакції</li>
-                                        <li>Велика настільна книга</li>
-                                        <li>Звітність у «СОТА»</li>
-                                    </template>
-                                </package>
-                            </div>
-                            <div class="col-md-4 col-lg-3 order__package">
-                                <package article="ep-dtkt_pp" class="dddd">
-                                    <template v-slot:title>ПАКЕТ <br> «ПРОФІ ПЛЮС»</template>
-                                    <template v-slot:gifts>
-                                        <li>Відеозапис Великого бухгалтерського семінару</li>
-                                        <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
-                                    </template>
-                                    <template v-slot:abilities>
-                                        <li>Щоденна добірка новин</li>
-                                        <li>«Документи за тиждень»</li>
-                                        <li>«Запитання і відповіді» за тиждень</li>
-                                        <li>Розумний податковий календар</li>
-                                        <li>База нормативних документів</li>
-                                        <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
-                                        <li>Сервіс «Мій кабінет»</li>
-                                        <li>Пакет «Тематичні розсилки»</li>
-                                        <li>Електронний журнал «Дебет-Кредит»</li>
-                                        <li>Щомісячні спецвипуски</li>
-                                        <li>Доступ до архівних номерів на сайті</li>
-                                        <li>Сервіс «Постав запитання редакції»</li>
-                                        <li>Вебінари редакції</li>
-                                        <li>Велика настільна книга</li>
-                                        <li>Звітність у «СОТА»</li>
-                                        <li>Бухгалтерська відеоплатформа 7eminar.ua</li>
-                                    </template>
-                                </package>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section class="landing-section landing-section--advantages">
-                <div class="container">
-                    <h2 class="landing-section__title landing-title"><span>Переваги</span></h2>
-                    <div class="advantages">
-                        <advantages></advantages>
-                    </div>
-                </div>
-            </section>
-            <section class="landing-section landing-section--slider">
-                <div class="container">
-                    <h2 class="landing-section__title landing-title"><span>Вас може зацікавити</span></h2>
-
-                        <products-slider></products-slider>
-
-                </div>
-            </section>
-        </div>
-    </div>
+    <landing name="dtkt" advantages="epressa_add_dtkt" class="landing-dtkt">
+        <template v-slot:about_description>
+            <p>«Дебет-Кредит» – бухгалтерський сервіс №1, який об'єднує бухгалтерів з усієї України. Щотижня він пропонує підбірку аналітичних статей, підготовлених висококваліфікованими фахівцями редакції, бухгалтерами-практиками, юристами, фахівцями держорганів.
+            </p>
+            <p>У ньому регулярно з’являється інформація про нові закони, нормативно-правові акти з докладними коментарями та практичними рекомендаціями. І разом з тим сервіс надає доступ до сотень готових відповідей у базі онлайн-консультацій. «Дебет-Кредит» – це джерело вашої абсолютної впевненості у власних діях.</p>
+        </template>
+        <template v-slot:packages_list>
+            <landing-package-row>
+                    <landing-package article="ep-dtkt_tr">
+                        <template v-slot:title>ПАКЕТ <br> «ТЕМАТИЧНІ РОЗСИЛКИ»</template>
+                        <template v-slot:gifts>
+                            <li>Відеозапис Великого бухгалтерського семінару</li>
+                            <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
+                        </template>
+                        <template v-slot:abilities>
+                            <li>Регулярні періодичні добірки – у вашій поштовій скрині</li>
+                            <li>Обирайте з 12 можливих тем те, що вам зараз дійсно потрібно</li>
+                        </template>
+                    </landing-package>
+                    <landing-package article="ep-dtkt_ma">
+                        <template v-slot:title>ПАКЕТ <br> «МІЙ АСИСТЕНТ»</template>
+                        <template v-slot:gifts>
+                            <li>Відеозапис Великого бухгалтерського семінару</li>
+                            <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
+                        </template>
+                        <template v-slot:abilities>
+                            <li>Щоденна добірка новин</li>
+                            <li>«Документи за тиждень»</li>
+                            <li>«Запитання і відповіді» за тиждень</li>
+                            <li>Розумний податковий календар</li>
+                            <li>База нормативних документів</li>
+                            <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
+                            <li>Сервіс «Мій кабінет»</li>
+                        </template>
+                    </landing-package>
+                    <landing-package article="ep-dtkt_ek">
+                        <template v-slot:title>ПАКЕТ <br> «ЕКСПЕРТ»</template>
+                        <template v-slot:gifts>
+                            <li>Відеозапис Великого бухгалтерського семінару</li>
+                            <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
+                        </template>
+                        <template v-slot:abilities>
+                            <li>Щоденна добірка новин</li>
+                            <li>«Документи за тиждень»</li>
+                            <li>«Запитання і відповіді» за тиждень</li>
+                            <li>Розумний податковий календар</li>
+                            <li>База нормативних документів</li>
+                            <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
+                            <li>Сервіс «Мій кабінет»</li>
+                            <li>Пакет «Тематичні розсилки»</li>
+                            <li>Електронний журнал «Дебет-Кредит»</li>
+                            <li>Щомісячні спецвипуски</li>
+                            <li>Доступ до архівних номерів на сайті</li>
+                        </template>
+                    </landing-package>
+            </landing-package-row>
+            <landing-package-row>
+                    <landing-package article="ep-dtkt_pr" class="dddd">
+                        <template v-slot:title>ПАКЕТ <br> «ПРОФІ»</template>
+                        <template v-slot:gifts>
+                            <li>Відеозапис Великого бухгалтерського семінару</li>
+                            <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
+                        </template>
+                        <template v-slot:abilities>
+                            <li>Щоденна добірка новин</li>
+                            <li>«Документи за тиждень»</li>
+                            <li>«Запитання і відповіді» за тиждень</li>
+                            <li>Розумний податковий календар</li>
+                            <li>База нормативних документів</li>
+                            <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
+                            <li>Сервіс «Мій кабінет»</li>
+                            <li>Пакет «Тематичні розсилки»</li>
+                            <li>Електронний журнал «Дебет-Кредит»</li>
+                            <li>Щомісячні спецвипуски</li>
+                            <li>Доступ до архівних номерів на сайті</li>
+                            <li>Сервіс «Постав запитання редакції»</li>
+                            <li>Вебінари редакції</li>
+                            <li>Велика настільна книга</li>
+                            <li>Звітність у «СОТА»</li>
+                        </template>
+                    </landing-package>
+                    <landing-package article="ep-dtkt_pp">
+                        <template v-slot:title>ПАКЕТ <br> «ПРОФІ ПЛЮС»</template>
+                        <template v-slot:gifts>
+                            <li>Відеозапис Великого бухгалтерського семінару</li>
+                            <li>Смартфон <a href="/" target="_blank">Meizu M5C</a></li>
+                        </template>
+                        <template v-slot:abilities>
+                            <li>Щоденна добірка новин</li>
+                            <li>«Документи за тиждень»</li>
+                            <li>«Запитання і відповіді» за тиждень</li>
+                            <li>Розумний податковий календар</li>
+                            <li>База нормативних документів</li>
+                            <li>Підсумковий випуск «Дебет-Кредит» за тиждень</li>
+                            <li>Сервіс «Мій кабінет»</li>
+                            <li>Пакет «Тематичні розсилки»</li>
+                            <li>Електронний журнал «Дебет-Кредит»</li>
+                            <li>Щомісячні спецвипуски</li>
+                            <li>Доступ до архівних номерів на сайті</li>
+                            <li>Сервіс «Постав запитання редакції»</li>
+                            <li>Вебінари редакції</li>
+                            <li>Велика настільна книга</li>
+                            <li>Звітність у «СОТА»</li>
+                            <li>Бухгалтерська відеоплатформа 7eminar.ua</li>
+                        </template>
+                    </landing-package>
+            </landing-package-row>
+        </template>
+    </landing>
 </template>
 
 <script>
 import Page from '../../core/Page'
-import Package from './Package'
-import Advantages from  './Advantages'
-import ProductsSlider from  './ProductsSlider'
 
 export default {
     mixins: [Page],
-    components: {Package,Advantages,ProductsSlider},
-    data:()=>({
-        showList: true
-    }),
 }
 </script>

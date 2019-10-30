9cede348ca976d3b1b731a299ddc9111e8dbca27
Artem KBS <artem4342232@gmail.com>
2019-10-30 12:32:27 +0200

    added new pages

--- epress/dtkt_october2019_3/src/component.vue
+++ epress/dtkt_october2019_3/src/component.vue
@@ -1,13 +1,13 @@
 <template>
-    <landing name="dtkt_october2019_1" advantages="epressa_add_dtkt" class="landing-dtkt_october2019_1">
+    <landing name="dtkt_october2019_3" advantages="epressa_add_dtkt" class="landing-dtkt_october2019_3">
         <template v-slot:cover>
             <div class="dtkt-promo">
                 <div class="landing-section">
                     <div class="container container--august">
                         <div class="cover">
                              <picture class="cover__img">
-                                 <source :srcset="mix('images/head_coffe.webp')" type="image/webp">
-                                 <img :src="mix('images/head_coffe.jpg')" class="img-fluid" alt="coffe">
+                                 <source :srcset="mix('images/head_tea_1.webp')" type="image/webp">
+                                 <img :src="mix('images/head_tea_1.jpg')" class="img-fluid" alt="coffe">
                              </picture>
                         </div>
                     </div>
@@ -20,23 +20,23 @@
         </template>
         <template v-slot:price-title>
             <div>
-                <h2 class="landing-title landing-title--columns"><span>АКЦІЙНА ПРОПОЗИЦІЯ ДІЄ ДО 11 ЖОВТНЯ</span></h2>
+                <h2 class="landing-title landing-title--columns"><span>АКЦІЙНА ПРОПОЗИЦІЯ ДІЄ ДО 31 ЖОВТНЯ</span></h2>
             </div>
         </template>
         <template v-slot:action-due>
 
-            <p class="landing-section__action">Немає роботи без кави! Отримайте все необхідне для створення ідеальної атмосфери, передплативши бухгалтерський сервіс «Дебет-Кредит».</p>
+            <p class="landing-section__action">Гарячий чай зігріє, а бухгалтерський сервіс «Дебет-Кредит» допоможе у роботі. І це ще не всі подарунки ;)</p>
 
          </template>
 
         <template v-slot:packages_list>
             <landing-package-row>
-                    <landing-package article="ep-dtkt_ek_1004" >
+                    <landing-package article="ep-dtkt_ek_2910" >
                         <template v-slot:title>ЕКСПЕРТ<br>та подарунок</template>
                         <template v-slot:period><div class="period">січень – червень 2020</div> </template>
                         <template v-slot:gifts>
-                            <li>online-трансляція та запис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
-                            <li class="no-active">гейзерна кавоварка</li>
+                            <li>відеозапис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
+                            <li class="no-active">електрочайник</li>
                         </template>
                         <template v-slot:abilities>
                             <li>Щоденна добірка новин</li>
@@ -56,18 +56,18 @@
                             <li class="package__abilities-item--noactive">Звітність у «СОТА»</li>
                         </template>
                         <template v-slot:old-price>
-                            <div class="package__old-price" style="text-decoration: line-through">1813 грн</div>
+                            <div class="package__old-price" style="text-decoration: line-through">1313 грн</div>
                         </template>
                     </landing-package>
 
 
 
-                    <landing-package article="ep-dtkt_ekp_1004" class="package-plus">
+                    <landing-package article="ep-dtkt_ekp_2910" class="package-plus">
                         <template v-slot:title>ЕКСПЕРТ + 7eminar.ua<br>та подарунок</template>
                         <template v-slot:period><div class="period">січень – червень 2020</div> </template>
                         <template v-slot:gifts>
-                            <li>online-трансляція та запис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
-                            <li class="no-active">гейзерна кавоварка</li>
+                            <li>відеозапис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
+                            <li class="no-active">електрочайник</li>
                         </template>
                         <template v-slot:abilities>
                             <li>Щоденна добірка новин</li>
@@ -88,16 +88,16 @@
                             <li class="package__abilities-item-platform">Бухгалтерська відеоплатформа  <a href="https://7eminar.ua" target="_blank">7eminar.ua</a>: понад 90 курсів та вебінарів для перегляду (січень – червень 2020 р.)</li>
                         </template>
                         <template v-slot:old-price>
-                            <div class="package__old-price" style="text-decoration: line-through">3811 грн</div>
+                            <div class="package__old-price" style="text-decoration: line-through">2312 грн</div>
                         </template>
                     </landing-package>
 
-                <landing-package article="ep-dtkt_pr_1004">
+                <landing-package article="ep-dtkt_pr_2910">
                     <template v-slot:title>ПРОФІ<br>та подарунки</template>
                     <template v-slot:period><div class="period">січень – червень 2020</div> </template>
                     <template v-slot:gifts>
-                        <li>online-трансляція та запис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
-                        <li><a href="https://rozetka.com.ua/maestro_mr_1667_6/p10017018/" target="_blank"> гейзерна кавоварка</a></li>
+                        <li>відеозапис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
+                        <li><a href="https://protovar.com.ua/Arita-AKT-5201--nerw-stal" target="_blank"> електрочайник</a></li>
                     </template>
                     <template v-slot:abilities>
                         <li>Щоденна добірка новин</li>
@@ -117,16 +117,16 @@
                         <li>Звітність у «СОТА»</li>
                     </template>
                     <template v-slot:old-price>
-                        <div class="package__old-price" style="text-decoration: line-through">2691 грн</div>
+                        <div class="package__old-price" style="text-decoration: line-through">2291 грн</div>
                     </template>
                 </landing-package>
 
-                <landing-package article="ep-dtkt_prp_1004" class="package-plus package-plus-top">
+                <landing-package article="ep-dtkt_pp_2910" class="package-plus package-plus-top">
                     <template v-slot:title>ПРОФІ + 7eminar.ua<br>та подарунки</template>
                     <template v-slot:period><div class="period">січень – червень 2020</div> </template>
                     <template v-slot:gifts>
-                        <li>online-трансляція та запис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
-                        <li><a href="https://rozetka.com.ua/maestro_mr_1667_6/p10017018/" target="_blank"> гейзерна кавоварка</a></li>
+                        <li>відеозапис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
+                        <li><a href="https://protovar.com.ua/Arita-AKT-5201--nerw-stal" target="_blank">електрочайник</a></li>
                     </template>
                     <template v-slot:abilities>
                         <li>Щоденна добірка новин</li>
@@ -147,7 +147,7 @@
                         <li class="package__abilities-item-platform">Бухгалтерська відеоплатформа  <a href="https://7eminar.ua" target="_blank">7eminar.ua</a>:  понад 90 курсів та вебінарів для перегляду (січень – червень 2020 р.)</li>
                     </template>
                     <template v-slot:old-price>
-                        <div class="package__old-price" style="text-decoration: line-through">4689 грн</div>
+                        <div class="package__old-price" style="text-decoration: line-through">3191 грн</div>
                     </template>
                 </landing-package>
 
---
+++ epress/msfz-liga_october2019_2/src/component.vue
@@ -0,0 +1,59 @@
+<template>
+    <landing name="msfz-liga_october2019_2" advantages="epressa_add_msfz-liga" class="landing-msfz-liga_october2019_2">
+        <template v-slot:cover>
+            <div class="dtkt-promo">
+                <div class="landing-section">
+                    <div class="container container--august">
+                        <div class="cover">
+                            <picture class="cover__img">
+                                <source :src="mix('images/head_tea_6.webp')" type="image/webp">
+                                <img :src="mix('images/head_tea_6.jpg')" class="img-fluid" alt="coffe">
+                            </picture>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </template>
+        <template v-slot:about>
+            <div>
+            </div>
+        </template>
+        <template v-slot:price-title>
+            <div>
+                <h2 class="landing-title landing-title--columns"><span>АКЦІЙНА ПРОПОЗИЦІЯ ДІЄ ДО 31 ЖОВТНЯ</span></h2>
+            </div>
+        </template>
+        <template v-slot:action-due>
+
+            <p class="landing-section__action">Гарячий чай зігріє, а електронне видання «Вісник МСФЗ» допоможе у роботі. І це ще не всі подарунки ;)</p>
+
+        </template>
+        <template v-slot:packages_list>
+            <landing-package-row>
+                <landing-package article="ep-msfz-liga">
+                    <template v-slot:title>Вісник МСФЗ <br> та подарунки</template>
+                    <template v-slot:gifts>
+                        <li>відеозапис<br> <a href="https://7eminar.com/08102019" target="_blank">Великого Квартального Семінару</a></li>
+                        <li><a href="https://protovar.com.ua/Arita-AKT-5201--nerw-stal" target="_blank" > електрочайник</a></li>
+                    </template>
+                    <template v-slot:abilities>
+                        <li>Електронне видання</li>
+                        <li>Функція «Задати питання автору»</li>
+                        <li>Доступ до накопиченого архіву видання</li>
+                        <li>Щоквартальна додаткова добірка ТОП-25 аналітичних матеріалів</li>
+                        <li>Гіперпосилання з матеріалів видання на документи й продукти ЛІГА:ЗАКОН</li>
+                        <li>«Довідник МСФЗ»</li>
+                    </template>
+                </landing-package>
+            </landing-package-row>
+        </template>
+    </landing>
+</template>
+
+<script>
+import Page from '../../core/Page'
+
+export default {
+    mixins: [Page],
+}
+</script>

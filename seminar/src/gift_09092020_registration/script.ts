ce66626db67dc17e330b956c31b9a76fc057da70
Artem Panuta <artem4342232@gmail.com>
2020-09-02 15:24:57 +0300

    fixed pages

---
+++ seminar/src/gift_09092020_registration/script.ts
@@ -0,0 +1,25 @@
+
+import jQuery from "jquery";
+import 'bootstrap';
+
+
+jQuery(($) => {
+
+
+    const viewport = $("meta[name=viewport]");
+    viewport.attr('content', 'width=device-width,initial-scale=1');
+
+
+    const loaderPage = $('#loader-page');
+    loaderPage.delay(350).fadeOut('slow');
+
+
+
+
+
+});
+
+
+
+
+

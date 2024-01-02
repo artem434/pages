68c56abcc7f3b57c1c58f1be2d5bc8262d0c89a2
artem <artem43422322@gmail.com>
2024-01-02 08:48:21 +0000

    fix

--- gf_sturtap_web/export.php
+++ gf_sturtap_web/export.php
@@ -35,7 +35,7 @@ $data['entry.648990995'] = isset($_POST['ref']) ? $_POST['ref'] : false;
 
 
 // теперь подготовим данные для отправки в гугл форму
-$url = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLSfDZyrgCmyFwTkAkNaWLfD9CVqyjmEb-K2OOolAkp00zd4G6w/formResponse'; // куда слать, это атрибут action у гугл формы
+$url = 'https://docs.google.com/forms/u/1/d/e/1FAIpQLSehb17pvoH806Q-wy7GU3KwMRmiJoRuK8c8t3EbMT4BrOWpvg/formResponse'; // куда слать, это атрибут action у гугл формы
 
 
 $data = http_build_query($data); // теперь сериализуем массив данных в строку для отправки

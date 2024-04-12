ea18e950d1e9d4223c9c7848bc1f51f4a4b71adc
artem <artem43422322@gmail.com>
2024-04-12 15:45:34 +0100

    fix

--- ceo/new2/js/main.js
+++ ceo/new2/js/main.js
@@ -35,7 +35,7 @@ $.get("https://ipapi.co/json/", function (obj) {
     separateDialCode: false,
     nationalMode: false,
     initialCountry: obj.country_code,
-    preferredCountries: ["ua", "kz"],
+    preferredCountries: ["pl", "ua"],
   });
 });
 
@@ -156,33 +156,6 @@ const lineSlider = new Swiper(".line__slider", {
   },
 });
 
-const studentSlider = new Swiper(".students__block", {
-  speed: 4000,
-  loop: true,
-  // freeMode: true,
-  spaceBetween: 34,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  freeModeMomentum: false,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  },
-  breakpoints: {
-    1260: {
-      speed: 0,
-      autoplay: false,
-      loop: false,
-      spaceBetween: 4,
-    },
-  },
-  on: {
-    init: function () {
-      lazyLoadInstance.update();
-    },
-  },
-});
-
 const clientsSlider = new Swiper(".clients__slider", {
   loop: true,
   spaceBetween: 24,
@@ -207,6 +180,41 @@ const clientsSlider = new Swiper(".clients__slider", {
     },
   },
 });
+
+// const slider = new Swiper('.slider', {
+// 	speed: 8000,
+// 	spaceBetween: 20,
+// 	loop: true,
+// 	freeMode: true,
+// 	centeredSlides: true,
+// 	slidesPerView: 2,
+// 	freeModeMomentum: false,
+// 	pagination: {
+// 		el: ".swiper-pagination",
+// 		dynamicBullets: true,
+// 	},
+// 	navigation: {
+// 		nextEl: ".swiper-button-next",
+// 		prevEl: ".swiper-button-prev",
+// 	},
+// 	autoplay: {
+// 		delay: 0,
+// 		disableOnInteraction: false
+// 	},
+// 	on: {
+// 		init: function () {
+// 			lazyLoadInstance.update();
+// 		},
+// 	},
+// 	breakpoints: {
+// 		760: {
+// 			slidesPerView: 4,
+// 		},
+// 		1260: {
+// 			slidesPerView: 5,
+// 		},
+// 	},
+// });
 
 $(".item__more, .program .item__title").on("click", function () {
   $(this).parent().toggleClass("active");
@@ -261,41 +269,4 @@ function hidePackagesBtn() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    var modal = document.getElementById("modal-video");
-    if (modal) {
-      modal.style.display = "block";
-      setTimeout(function () {
-        modal.style.opacity = "1";
-      }, 50);
-    }
-  }, 5000); // 5000 миллисекунд = 5 секунд
-});
-document.addEventListener("DOMContentLoaded", function () {
-  var modal = document.getElementById("modal-video");
-  if (modal) {
-    var minutesDisplay = document.getElementById("minutes");
-    var secondsDisplay = document.getElementById("seconds");
-
-    var totalSeconds = 120; // 2 минуты
-
-    var timer = setInterval(function () {
-      var minutes = Math.floor(totalSeconds / 60);
-      var seconds = totalSeconds % 60;
-
-      minutesDisplay.innerHTML = pad(minutes) + "<span>хв</span>";
-      secondsDisplay.innerHTML = pad(seconds) + "<span>сек</span>";
-
-      if (totalSeconds <= 0) {
-        clearInterval(timer);
-      } else {
-        totalSeconds--;
-      }
-    }, 1000);
-  }
-});
-
-function pad(val) {
-  return val > 9 ? val : "0" + val;
-}
+hidePackagesBtn();

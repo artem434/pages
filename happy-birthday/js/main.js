e685703f9c9e5153caff1d5dab7f1ec0b4293a6e
artem <artem43422322@gmail.com>
2024-05-21 16:09:25 +0100

    fix

--- happy-birthday/js/main.js
+++ happy-birthday/js/main.js
@@ -41,15 +41,17 @@ $.get("https://ipapi.co/json/", function (obj) {
 
 $("form [type=sumbit]").on("click", function (e) {
   e.preventDefault();
-  var form = $(this).closest("form");
-  form.addClass("loading");
-  setTimeout(function () {
-    form.submit();
-  }, 1000);
+  console.log("1");
+  // var form = $(this).closest("form");
+  // form.addClass("loading");
+  // setTimeout(function () {
+  //   form.submit();
+  // }, 1000);
 });
 
 function validate(formid) {
   var output = false;
+  console.log("1");
   form = $(formid);
   form.addClass("loading");
   form.find("input[name=name]").focus();
@@ -194,33 +196,25 @@ $(".header__burger").on("click", function () {
   $(".header").toggleClass("active");
   $("body").toggleClass("fixed");
 });
-// Iterate over each select element
+
 $("select").each(function () {
-  // Cache the number of options
   var $this = $(this),
     numberOfOptions = $(this).children("option").length;
 
-  // Hides the select element
   $this.addClass("s-hidden");
 
-  // Wrap the select element in a div
   $this.wrap('<div class="select"></div>');
 
-  // Insert a styled div to sit over the top of the hidden select element
   $this.after('<div class="styledSelect"></div');
 
-  // Cache the styled div
   var $styledSelect = $this.next("div.styledSelect");
 
-  // Show the first select option in the styled div
   $styledSelect.text($this.children("option").eq(0).text());
 
-  // Insert an unordered list after the styled div and also cache the list
   var $list = $("<ul />", {
     class: "options",
   }).insertAfter($styledSelect);
 
-  // Insert a list item into the unordered list for each select option, starting from the second option (index 1)
   for (var i = 1; i < numberOfOptions; i++) {
     $("<li />", {
       text: $this.children("option").eq(i).text(),
@@ -228,30 +222,25 @@ $("select").each(function () {
     }).appendTo($list);
   }
 
-  // Cache the list items
   var $listItems = $list.children("li");
 
-  // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
   $styledSelect.click(function (e) {
     e.stopPropagation();
     $("div.styledSelect.active").each(function () {
       $(this).removeClass("active").next("ul.options").hide();
     });
+
     $(this).toggleClass("active").next("ul.options").toggle();
   });
 
-  // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
-  // Updates the select element to have the value of the equivalent option
   $listItems.click(function (e) {
     e.stopPropagation();
     $styledSelect.text($(this).text()).removeClass("active");
     $this.val($(this).attr("rel"));
+    $(".select").removeClass("error");
     $list.hide();
-
-    /* alert($this.val()); Uncomment this for demonstration! */
   });
 
-  // Hides the unordered list when clicking outside of it
   $(document).click(function () {
     $styledSelect.removeClass("active");
     $list.hide();
@@ -261,23 +250,18 @@ $("select").each(function () {
 var $listItems = $("ul.options li");
 var $form = $("form.form.zoho_url");
 var $landingIdInput = $form.find('input[name="landing_id"]');
-// Добавляем обработчик события click для каждого элемента <li>
+
 $listItems.click(function () {
-  // Получаем значение атрибута "rel" выбранного элемента и выводим его в консоль
   var relValue = $(this).attr("rel");
   $landingIdInput.val(relValue);
 });
 $(".program-btn").click(function () {
-  // Получить значение атрибута "data-rel" из элемента .program-btn
   var dataRel = $(this).data("rel");
 
-  // Найти соответствующий элемент <select>
   var $select = $("select");
 
-  // Установить выбранный вариант
   $select.val(dataRel);
 
-  // Обновить отображение стилизированного элемента
   var $styledSelect = $select.next("div.styledSelect");
   $styledSelect.text($select.children("option:selected").text());
 });

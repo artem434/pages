d3f333d5caf35f186d3335a6c87271fd72af5abc
artem <artem43422322@gmail.com>
2024-05-23 20:14:16 +0100

    fix

---
+++ ceo/offer_ceo/program.php
@@ -0,0 +1,240 @@
+<section class="program" id="program">
+      <div class="program__container">
+        <h2 class="program__title">
+          <span>Структура</span> програми
+        </h2>
+        <div class="program__items">
+          <div class="item">
+            <p class="item__number">01</p>
+            <h4 class="item__title">Роль і функції СЕО</h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Роль, функціонал та очікувані результати роботи СЕО.</li>
+              <li>Еволюція ролі CEO з розвитком компанії</li>
+              <li>Взаємодія CEO із ключовими центрами відповідальності</li>
+              <li>Баланс між зовнішнім та внутрішнім середовищем бізнесу</li>
+              <li>Принципи ефективного лідерства</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">02</p>
+            <h4 class="item__title"> Особиста ефективність СЕО </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Особиста ефективність СЕО</li>
+              <li>Особистий та корпоративний тайм-менеджмент</li>
+              <li>Лідерство та принципи створення сильної компанії</li>
+              <li>Зміна моделі поведінки CEO на різних етапах розвитку компанії</li>
+              <li>Робота з персональним асистентом</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">03</p>
+            <h4 class="item__title"> Стратегія бізнесу і бізнес-планування </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Бізнес-модель та product-market fit</li>
+              <li>Стратегія масштабування компанії</li>
+              <li>Ринок та цільовий споживач</li>
+              <li>Методи дослідження ринку</li>
+              <li>Unique Selling Proposition (USP) та конкурентна стратегія</li>
+              <li>Методи аналізу конкурентів</li>
+              <li>Benchmarking</li>
+              <li>Продукт та управління шляхом клієнта (Customer Journey)</li>
+              <li>Сегментація цільової аудиторії та продуктів компанії</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">04</p>
+            <h4 class="item__title"> Система залучення та втримання клієнтів </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Стратегія залучення та утримання клієнтів</li>
+              <li>Inbound та outbound підходи у залученні клієнтів</li>
+              <li>Brand-маркетинг та performance-маркетинг</li>
+              <li>Юніт-економіка та моделювання маркетингових воронок</li>
+              <li>Сучасні інструменти Інтернет-маркетингу</li>
+              <li>Управління продажами та CRM</li>
+              <li>Відмінність продажів у B2C та B2B сегментах</li>
+              <li>Технології та стандарти продажів</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">05</p>
+            <h4 class="item__title"> Організаційна структура і регламентація зон відповідальності </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Основні принципи побудови ефективної організаційної структури</li>
+              <li>Види організаційних структур</li>
+              <li>C-level та ключові центри відповідальності в організаційній структурі</li>
+              <li>Еволюція організаційної структури з розвитком компанії</li>
+              <li>Регламентація зон відповідальності в організаційній структурі</li>
+              <li>Карти посади та очікувані результати роботи співробітників</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">06</p>
+            <h4 class="item__title"> Бізнес-процеси і організація ефективної взаємодії в команді </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Опис бізнес-процесів</li>
+              <li>Формат BPMN 2.0</li>
+              <li>Принцип внутрішнього клієнта</li>
+              <li>Підвищення якості взаємодії у команді</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">07</p>
+            <h4 class="item__title"> HR-менеджмент і розвиток корпоративної культури </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Роль HR-функції у компанії</li>
+              <li>Управління якісним складом команди</li>
+              <li>Рекрутинг</li>
+              <li>Система вступної адаптації та атестації</li>
+              <li>Управління мотивацією співробітників</li>
+              <li>Оцінка та розвиток персоналу</li>
+              <li>Розвиток керівників</li>
+              <li>Корпоративна культура та принципи її формування</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">08</p>
+            <h4 class="item__title"> Розвиток керівників </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Принципи ефективного керівника</li>
+              <li>Система розвитку якості процесу управління</li>
+              <li>Ситуаційне лідерство</li>
+              <li>Управління за цілями (MBO)</li>
+              <li>Делегування</li>
+              <li>Емоційне лідерство (EQ)</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">09</p>
+            <h4 class="item__title"> Бренд-маркетинг </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Сила бренду: управління впізнаваністю та лояльністю до бренду</li>
+              <li>ATL та BTL реклама</li>
+              <li>PR</li>
+              <li>SMM для розвитку бренду</li>
+              <li>Контент-маркетинг</li>
+              <li>Інфлюєнсер-маркетинг</li>
+              <li>Event-маркетинг</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">10</p>
+            <h4 class="item__title"> Performance-маркетинг і управління продажами </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Завдання performance-маркетингу</li>
+              <li>Управління психологією цільового клієнта: Awarness, Consideration, Conversion</li>
+              <li>Воронка залучення клієнтів</li>
+              <li>Методи лідогенерації</li>
+              <li>CRM-система</li>
+              <li>Керування продажами</li>
+              <li>Технології та стандарти продажів</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">11</p>
+            <h4 class="item__title"> Управління фінансами </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Головні фінансові звіти бізнесу: Balance, P&L та Cashflow</li>
+              <li>Впровадження та автоматизація управлінського обліку</li>
+              <li>Роль CFO та процес управління фінансами</li>
+              <li>Управління грошовими потоками</li>
+              <li>Фінансове планування та бюджетування</li>
+              <li>Контролінг</li>
+              <li>Ключові показники фінансової ефективності</li>
+              <li>Фінансова модель компанії</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">12</p>
+            <h4 class="item__title"> Операційне управління </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Регулярний менеджмент</li>
+              <li>Правила ефективного проведення управлінських заходів</li>
+              <li>Побудова dashboard-ів та система внутрішньої звітності</li>
+              <li>Підходи до управління проектами</li>
+              <li>Забезпечення дисципліни виконання завдань</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">13</p>
+            <h4 class="item__title"> Стратегічне планування і забезпечення дисципліни реалізації стратегії </h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Ритм заходів щодо стратегічного планування</li>
+              <li>Бачення майбутнього компанії та CEO view</li>
+              <li>Правила постановки цілей</li>
+              <li>Взаємозв'язок між цілями бізнесу</li>
+              <li>Метод OKR у стратегічному плануванні</li>
+              <li>Забезпечення дисципліни реалізації стратегічних планів</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">14</p>
+            <h4 class="item__title">Залучення інвестицій <span>[пакет участі PREMIUM]</span></h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Капіталізація бізнесу</li>
+              <li>Джерела залучення інвестицій</li>
+              <li>Оцінка вартості інвестицій</li>
+              <li>Інвестиційні ризики</li>
+              <li>Робота з інвесторами</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">15</p>
+            <h4 class="item__title">Міжнародний розвиток <span>[пакет участі PREMIUM]</span></h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Бізнес-модель міжнародних продажів та вибір географії</li>
+              <li>Побудова маркетингу на міжнародних ринках </li>
+              <li>Управління продажами на міжнародних ринках</li>
+              <li>Управління командою та організація запуску міжнародних продажів</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">16</p>
+            <h4 class="item__title">HR-маркетинг і бренд роботодавця <span> <br>[пакет участі PREMIUM]</span></h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Інструменти HR-маркетингу</li>
+              <li>Бренд компанії на ринку </li>
+              <li>Управління репутацією</li>
+              <li>EVP</li>
+            </ul>
+          </div>
+          <div class="item">
+            <p class="item__number">17</p>
+            <h4 class="item__title">Управління проектами <span>[пакет участі PREMIUM]</span></h4>
+            <div class="item__more"></div>
+            <ul class="item__info">
+              <li>Методи управлiння проектами</li>
+              <li>Waterfall-підхід до управління проєктами</li>
+              <li>Agile та SCRUM</li>
+              <li>Автоматизація управління проєктами</li>
+            </ul>
+          </div>
+        </div>
+        <div class="block">
+          <div class="block__content">
+            <h2 class="block__title">Авторська методологія <b>та система навчання</b>
+            </h2>
+            <p class="block__text">дозволять у найкоротший термін отримати практичні інструменти й алгоритми з побудови системного управління компанією та опанувати навички топ-управлінця.</p>
+          </div>
+          <div class="block__buttons">
+            <p class="block__buttons_text">Залиш заявку, щоб забронювати участь.</p>
+            <a class="block__buttons_btn" href="#register">ЗАРЕЄСТРУВАТИСЯ</a>
+          </div>
+        </div>
+      </div>
+    </section>

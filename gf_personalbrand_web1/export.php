5deeb16d35fe817c60ca681bf1a799adc4429ac4
artem <artem43422322@gmail.com>
2024-12-11 07:14:23 +0000

    fix

--- gf_personalbrand_web1/export.php
+++ gf_personalbrand_web1/export.php
@@ -35,7 +35,7 @@ $data['entry.648990995'] = isset($_POST['ref']) ? $_POST['ref'] : false;
 
 
 // теперь подготовим данные для отправки в гугл форму
-$url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf8rlS4u1Y7goKt_Te7eZqYktyW0IO236e5uJRQCiZsoaYMtw/formResponse"; // куда слать, это атрибут action у гугл формы
+$url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdsIKZ-Ft4rOdK46HptVvjO9e5dtp5gAQ5B7zxfeER0ptmnwg/formResponse"; // куда слать, это атрибут action у гугл формы
 
 
 $data = http_build_query($data); // теперь сериализуем массив данных в строку для отправки

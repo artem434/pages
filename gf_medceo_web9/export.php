770634dc9a1a67f71d1ab65ee1046d186814dd4e
artem <artem43422322@gmail.com>
2025-03-08 13:14:19 +0000

    fix

--- gf_medceo_web9/export.php
+++ gf_medceo_web9/export.php
@@ -35,7 +35,7 @@ $data['entry.648990995'] = isset($_POST['ref']) ? $_POST['ref'] : false;
 
 
 // теперь подготовим данные для отправки в гугл форму
-$url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSenTt9A2LksyIzqQePkWzMfRI_koQz1SZxvGWFR6cR7CB2o2g/formResponse"; // куда слать, это атрибут action у гугл формы
+$url = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSe2LF2R1-sK_LZr04BCpYFalMKYMIB7wtE9NGsobZjxgvr6kg/formResponse"; // куда слать, это атрибут action у гугл формы
 
 
 $data = http_build_query($data); // теперь сериализуем массив данных в строку для отправки
